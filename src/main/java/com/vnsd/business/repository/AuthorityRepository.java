f59b7377629223c1520afd8967190ebb14b3db5b
DBIZ\nguyenn <nt.nguyen@lombardodier.com>
2019-08-24 15:08:37 +0200

    Initial application generated by JHipster-6.2.0

---
+++ src/main/java/com/vnsd/business/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.vnsd.business.repository;
+
+import com.vnsd.business.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
---
+++ src/main/java/com/vnsd/business/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.vnsd.business.service.dto;
